7182ab56796ebd657298a9b983ccb9437d7f8117
minjun1803 <minjun1803@korea.ac.kr>
2026-06-21 22:11:38 +0900

    chore: 심사용·앱 필수 파일만 유지, 내부 작업 이력 정리 + README 문체 정리

--- application_build/test_squircle.py
+++
@@ -1,6 +0,0 @@
-from PIL import Image, ImageDraw
-import os
-
-img = Image.open('/Users/minjun1803/Documents/Python/Project_Market_Dashboard/application_build/레알진짜끝.png')
-print("Image size:", img.size)
-
